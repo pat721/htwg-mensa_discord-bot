a674beea33b203c6d5deb9edee7b57a8ac57adc6
Patrick Kratzer <38780545+pat721@users.noreply.github.com>
2022-04-05 15:35:57 +0200

    restructure files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const config = require("./config.json");
+const config = require("../config.json");
 const { extractCommand, httpWebsiteRequest } = require("./helpers");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
